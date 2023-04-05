0f24f55dcf3514ba5563ccecdf9966af107c467d
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-05 19:30:10 +0600

    Set up the footer page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
       <h1 className="text-xl">Welcome to nextPage!</h1>
       <Outlet />
+      <Footer />
     </div>
   );
 }
