0b0a2a30a5c4b7c86f7f9398d884676028681cc9
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-06 01:11:50 +0600

    now i try to build the app with pagination

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { useLoaderData, useNavigation } from "react-router-dom";
 import Book from "./Book";
 import LoadingSpinner from "./LoadingSpinner";
@@ -7,13 +7,20 @@ const Books = () => {
   const { books } = useLoaderData();
   const navigation = useNavigation();
 
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage, setPostPerPage] = useState(4);
+
+  const lastPostIndex = currentPage * postsPerPage;
+  const firstPostIndex = lastPostIndex - postsPerPage;
+  const currentPosts = books.slice(firstPostIndex, lastPostIndex);
   if (navigation.state === "loading") {
     return <LoadingSpinner />;
   }
+
   return (
     <div className="my-container">
       <div className="grid gap-6 mb-8 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 justify-items-center">
-        {books.map((book, idx) => (
+        {currentPosts.map((book, idx) => (
           <Book key={idx} data={book} />
         ))}
       </div>
