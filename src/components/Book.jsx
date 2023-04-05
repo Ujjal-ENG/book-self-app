f4b9a884dcc74da7a78102df56806482b48987fe
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-05 21:56:19 +0600

    book data show using api

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,7 +1,23 @@
 import React from "react";
 
-const Book = () => {
-  return <div>Book</div>;
+const Book = ({ data }) => {
+  return (
+    <div className="overflow-hidden relative duration-300 transition-all transform hover:translate-y-2">
+      <img
+        src={data.image}
+        alt={data.title}
+        className="object-cover w-full h-56 md:h-64 xl:h-80"
+      />
+      <div className="bg-black bg-opacity-75 opacity-0 hover:opacity-100 duration-300 transition-opacity ease-in top-0 bottom-0 text-gray-300 absolute px-6 py-4 rounded-lg flex flex-col">
+        <h1 className="text-xl font-bold">{data.title}</h1>
+        <br />
+        <h1 className="text-xl font-bold">{data.subtitle}</h1>
+        <h4 className="text-2xl mt-auto  font-bold text-red-600">
+          price: {data.price}
+        </h4>
+      </div>
+    </div>
+  );
 };
 
 export default Book;
