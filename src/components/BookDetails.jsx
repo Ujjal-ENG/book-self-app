44115a0fabcc6ab36a62e10e9de554f0460c58d6
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-05 22:10:51 +0600

    successfully passed the data

--- src/components/BookDetails.jsx
+++ src/components/BookDetails.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
 
 const BookDetails = () => {
+  const bookData = useLoaderData();
+  console.log(bookData);
   return <div>BookDetails</div>;
 };
 
--- src/routes/App.routes.jsx
+++ src/routes/App.routes.jsx
@@ -23,7 +23,8 @@ export const router = createBrowserRouter([
       {
         path: "books/book-details/:id",
         element: <BookDetails />,
-        loader: async ({ params }) => fetch(``),
+        loader: async ({ params }) =>
+          fetch(`https://api.itbook.store/1.0/books/${params.id}`),
       },
       {
         path: "/about",
