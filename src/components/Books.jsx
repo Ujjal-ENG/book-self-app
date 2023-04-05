452314e39e6f77981886fc2eb2449de9f2830469
Ujjal Kumar Roy <ujjalroy7862@gmail.com>
2023-04-05 23:11:56 +0600

    done the loading sppiner shown using useNavigation hooks from react router dom

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,9 +1,15 @@
 import React from "react";
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigation } from "react-router-dom";
 import Book from "./Book";
+import LoadingSpinner from "./LoadingSpinner";
 
 const Books = () => {
   const { books } = useLoaderData();
+  const navigation = useNavigation();
+
+  if (navigation.state === "loading") {
+    return <LoadingSpinner />;
+  }
   return (
     <div className="my-container">
       <div className="grid gap-6 mb-8 md:grid-cols-3 lg:grid-cols-4 sm:grid-cols-2 justify-items-center">
